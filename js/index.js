340f358febb81aa056d878d99452ebe56277d65e
RajavarapuNageswarao <rajavarapumunna@gmail.com>
2024-04-12 19:16:41 +0530

    commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// //This is functions  are work down to up
+// //you can use Arrow functions or any other function same functionality
+// const Arrow = (name)=>{
+//   console.log(name + ",","Hii This is Arrow!")
+//   const AnotherArrow = (first_name)=>{
+//     console.log(first_name + ",","Hi This is AnotherArrow")
+//     const secondArrow = (last_name)=>{
+//       console.log(last_name + ",","This is for secondArrow");
+//     }
+//     secondArrow("Neena");
+//   }
+//   AnotherArrow("John");
+// }
+// Arrow("Name");
+
+
+const Arrow = (name)=> console.log(name + ",","Hii This is Arrow!")
+  const AnotherArrow = (first_name)=> console.log(first_name + ",","Hi This is AnotherArrow")
+    const secondArrow = (last_name)=>console.log(last_name + ",","This is for secondArrow");
+    secondArrow("Neena");
+  AnotherArrow("John");
+Arrow("Name");
